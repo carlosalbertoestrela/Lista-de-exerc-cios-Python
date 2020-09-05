fca7fbcb411a84b058ddd45c06795be1168b54a9
Carlos Alberto Estrela de Souza Junior <69488591+carlosalbertoestrela@users.noreply.github.com>
2020-09-05 15:58:04 -0300

    Atividade 19 finalizada

--- 02 Estrutura_de_Decisão/19.py
+++ 02 Estrutura_de_Decisão/19.py
@@ -5,35 +5,35 @@ Observando os termos no plural a colocação do "e", da vírgula entre outros. E
   326 = 3 centenas, 2 dezenas e 6 unidades
   12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
 """
-while True:
-    num = int(input('Digite um númro menor que 1000: '))
-    if 0 > num > 1000:
-        print('Número inválido')
-    else:
-        cent = num // 100
-        dezn = (num - (cent * 100)) // 10
-        unid = (num - (cent * 100) - (dezn * 10)) // 1
-        p_c = p_d = p_u = ''
-        if cent > 1:
-            p_c = 's'
-        if dezn > 1:
-            p_d = 's'
-        if unid > 1:
-            p_u = 's'
-        print(f'O número {num} tem', end='')
-        if cent > 0:
-            print(f' {cent} cemtena{p_c}', end='')
-            if dezn > 0 and unid > 0:
-                print(',', end='')
-            elif dezn > 0 or unid > 0:
-                print(' e', end='')
-            else:
-                print('.')
-        if dezn > 0:
-            print(f' {dezn} dezena{p_d}', end='')
-            if unid > 0:
-                print(' e', end='')
-            else:
-                print('.')
+
+num = int(input('Digite um númro menor que 1000: '))
+if 0 > num > 1000:
+    print('Número inválido')
+else:
+    cent = num // 100
+    dezn = (num - (cent * 100)) // 10
+    unid = (num - (cent * 100) - (dezn * 10)) // 1
+    p_c = p_d = p_u = ''
+    if cent > 1:
+        p_c = 's'
+    if dezn > 1:
+        p_d = 's'
+    if unid > 1:
+        p_u = 's'
+    print(f'O número {num} tem', end='')
+    if cent > 0:
+        print(f' {cent} cemtena{p_c}', end='')
+        if dezn > 0 and unid > 0:
+            print(',', end='')
+        elif dezn > 0 or unid > 0:
+            print(' e', end='')
+        else:
+            print('.')
+    if dezn > 0:
+        print(f' {dezn} dezena{p_d}', end='')
         if unid > 0:
-            print(f' {unid} unidade{p_u}.')
+            print(' e', end='')
+        else:
+            print('.')
+    if unid > 0:
+        print(f' {unid} unidade{p_u}.')
