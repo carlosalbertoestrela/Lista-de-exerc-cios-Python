167f63ee3e4dc542510890247151749cd319a73b
Carlos Alberto Estrela de Souza Junior <69488591+carlosalbertoestrela@users.noreply.github.com>
2020-09-05 19:56:34 -0300

    Atividade 23 finalizada definitiva

--- 02 Estrutura_de_Decisão/23.py
+++ 02 Estrutura_de_Decisão/23.py
@@ -2,10 +2,9 @@
  23) Faça um Programa que peça um número e informe se o número é inteiro ou decimal.
  Dica: utilize uma função de arredondamento.
 """
-num = str(input('Digite um número: '))
+num = float(input('Digite um número: '))
 print(f'{num} é ', end='')
-if ',' in num or '.' in num:
-    num = num.replace(",", ".")
-    print('Decimal')
-else:
+if num // 1 == num:
     print('Inteiro')
+else:
+    print('Decimal')
