f6446e738170858aedba54d68a1a7192f0c088a5
Carlos Alberto Estrela de Souza Junior <69488591+carlosalbertoestrela@users.noreply.github.com>
2020-09-07 00:29:03 -0300

    Atividade 10 corrigida e finalizada

--- 03 Estrutura_de_repetição/10.py
+++ 03 Estrutura_de_repetição/10.py
@@ -4,11 +4,13 @@
 """
 i = int(input('Digite o número de inicio: '))
 f = int(input('Digite o número final: '))
+print(f'Os númeors no intervalo entre {i} e {f} são: ', end='')
 if i > f:
     p = -1
+    for num in range(i-1, f, p):
+        print(f'{num}, ', end='')
 else:
     p = 1
-print(f'Os númeors no intervalo entre {i} e {f} são: ', end='')
-for num in range((i-1), f, p):
-    print(f'{num}, ', end='')
+    for num in range(i+1, f, p):
+        print(f'{num}, ', end='')
 print('fim')
